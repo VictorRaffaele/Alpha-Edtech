45d4d666a27eaa2125f8c755d60de8a205b028b8
victorraffaele <victorraffaele@gmail.com>
2022-06-14 21:40:58 -0300

    fix: bug in validate of user.js

--- Modulo JS/Atividade8/assets/script/user.js
+++ Modulo JS/Atividade8/assets/script/user.js
@@ -48,7 +48,7 @@ function register() {
 
 function confirm(e) {
 
-    if (e.data != "1" && e.data != "2" && e.data != "3" && e.data != "4" && e.data != "5" && e.data != "6" && e.data != "7" && e.data != "8" && e.data != "9" && e.data != null) {
+    if (e.data != "1" && e.data != "2" && e.data != "3" && e.data != "4" && e.data != "5" && e.data != "6" && e.data != "7" && e.data != "8" && e.data != "9" && e.data != "0" && e.data != null) {
         this.value = this.value.slice(0,-1);
     } 
 }
