43eb4e9fb1014a218ae0e214c79a16175d8d6a96
victorraffaele <victorraffaele@gmail.com>
2022-06-15 09:44:59 -0300

    feat: second print

--- Modulo JS/Atividade8/assets/script/objVoid.js
+++ Modulo JS/Atividade8/assets/script/objVoid.js
@@ -6,6 +6,7 @@ function show() {
     
     obj = {"Um atributo com espaços" : 1};
     console.log(obj["Um atributo com espaços"]);
+    console.log(Object.values(obj)[0]); 
 }
 
 btVisualizar.addEventListener("click", show);
