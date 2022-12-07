d3687dc3574600c1547f9323a6e163210f0a88fe
victorraffaele <victorraffaele@gmail.com>
2022-12-07 20:36:17 -0300

    feat: fetch on typescript api

--- Modulo React/Atividade2/my-form/src/form.tsx
+++ Modulo React/Atividade2/my-form/src/form.tsx
@@ -5,69 +5,82 @@ import {EmailValidator, NameValidator, PasswordValidator} from './validator/vali
 export function Form() {
     let [modalOpened, setModalOpened] = useState<boolean>(false);
     function tryEmail(email: any){
-        try {
-          new EmailValidator(email);
-        }
-        catch (error: any) {
-          throw new Error('Email: ' + error.message);
-        }
+      try {
+        new EmailValidator(email);
       }
-    
-      function tryName(name: any) {
-        try {
-          new NameValidator(name);
-        }
-        catch (error: any) {
-          throw new Error('Name: ' + error.message);
-        }
+      catch (error: any) {
+        throw new Error('Email: ' + error.message);
       }
+    }
     
-      function tryPass(pass: any) {
-        try {
-          new PasswordValidator(pass);
-        }
-        catch (error: any) {
-          throw new Error('Senha: ' + error.message);
-        }
+    function tryName(name: any) {
+      try {
+        new NameValidator(name);
       }
+      catch (error: any) {
+        throw new Error('Name: ' + error.message);
+      }
+    }
     
-      function submit(e: any) {
+    function tryPass(pass: any) {
+      try {
+        new PasswordValidator(pass);
+      }
+      catch (error: any) {
+        throw new Error('Senha: ' + error.message);
+      }
+    }
+    
+      async function submit(e: any) {
         const resultP: any = document.querySelector('#resultP');
         const email: any = document.querySelector("#input-email");
         const name: any = document.querySelector("#input-name");
         const password: any = document.querySelector("#input-pass");
-
         e.preventDefault();
         try {
           tryEmail(email.value);
           tryName(name.value);
           tryPass(password.value);
-          const usr = 
-          {
-            'email' : email.value,
-            'name' : name.value,
-            'pass': password.value
-          }
+          const usr = {
+            "email" : email.value,
+            "name" : name.value,
+            "password": password.value
+          };
+          console.log(JSON.stringify(usr));
+          const result = await fetch('http://localhost:8000/accounts/login', {
+            method: 'POST',  
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://localhost:8000',
+            'Access-Control-Allow-Credentials': 'true',
+            },
+            mode: 'no-cors',
+            body: JSON.stringify(usr),
+          }).then((resp) => {
+            if (!resp.ok) {
+              throw new Error("Error on response");
+            }
+            return resp.json();
+          });
           resultP.innerText = '';
-          console.log(usr);
           setModalOpened(true);
         } catch (error: any) {
           resultP.innerHTML = error.message;
         }
       }
-    
       return (
         <>
-            {modalOpened? <Modal position='fixed' opened={[modalOpened, setModalOpened]}/> : <></>}
-            <form onSubmit={submit}>
-                <label htmlFor='input-email'>Email:</label>
-                <input id='input-email' type='text'></input>
-                <label htmlFor='input-name'>Nome:</label>
-                <input id='input-name' type='text'></input>
-                <label htmlFor='input-pass'>Senha:</label>
-                <input id='input-pass' type='password'></input>
-                <button>Entrar</button>
-                <p id='resultP'></p>
+          {modalOpened? <Modal position='fixed' opened={[modalOpened, setModalOpened]}/> : <></>}
+          <form onSubmit={submit}>
+            <label htmlFor='input-email'>Email:</label>
+            <input id='input-email' type='text'></input>
+            <label htmlFor='input-name'>Nome:</label>
+            <input id='input-name' type='text'></input>
+            <label htmlFor='input-pass'>Senha:</label>
+            <input id='input-pass' type='password'></input>
+            <button>Entrar</button>
+            <p id='resultP'></p>
           </form>
         </>
       );
